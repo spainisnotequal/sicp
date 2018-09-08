3a7d98a5c92d0fa7af6ef4ca8f4e79a1bcf4fd81
spainisnotequal <spainisnotequal@gmail.com>
2018-09-08 11:44:41 +0200

    Add book examples of section 2.2.1

---
+++ chapter2/section2.2.scm
@@ -0,0 +1,79 @@
+;; ======================================================= ;;
+;; Section 2.2: Hierarchical Data and the Closure Property ;;
+;; ======================================================= ;;
+
+
+;; --------------- ;;
+;; List operations ;;
+;; --------------- ;;
+
+(list 1 2 3 4)
+
+(define one-through-four (list 1 2 3 4))
+
+(car one-through-four) ; 1
+(cdr one-through-four) ; (2 3 4)
+(car (cdr one-through-four)) ; 2
+(cdr (cdr one-through-four)) ; (3 4)
+(cdr (cdr (cdr (cdr one-through-four)))) ; ()
+
+(list-ref one-through-four 0) ; 1
+(list-ref one-through-four 1) ; 2
+(list-ref one-through-four 2) ; 3
+(list-ref one-through-four 3) ; 4
+(list-ref one-through-four 4) ; The object 4, passed as an argument to list-ref, is not in the correct range.
+
+(length one-through-four) ; 4
+
+(define odds (list 1 3 5 7 9))
+
+(append one-through-four odds) ; (1 2 3 4 1 3 5 7 9)
+
+
+;; ------------------ ;;
+;; Mapping over lists ;;
+;; ------------------ ;;
+
+
+(define (scale-list items factor)
+  (if (null? items)
+      '()
+      (cons (* (car items) factor)
+            (scale-list (cdr items) factor))))
+
+(scale-list (list 1 2 3 4 5) 10)
+
+
+;; "map" general procedure provided by the Scheme implementation
+;; -------------------------------------------------------------
+
+;; using it with a single list:
+(map (lambda(x) (* x 10)) (list 1 2 3 4 5))
+(map (lambda(x) (* x x)) (list 1 2 3 4 5))
+(map abs (list 1 -2 -3 4 5))
+
+;; using it with two or  more lists:
+(map + (list 1 2 3) (list 40 50 60) (list 700 800 900))
+
+
+;; "map" procedure for a single list can be implemented this way
+;; -------------------------------------------------------------
+
+(define (map proc items)
+  (if (null? items)
+      '()
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(map abs (list -10 2.5 -11.6 17))
+(map (lambda (x) (* x x)) (list 1 2 3 4))
+
+
+;; rewritting the "scale-list" procedure in terms of "map"
+;; -------------------------------------------------------
+
+(define (scale-list items factor)
+  (map (lambda (x) (* x factor))
+       items))
+
+(scale-list (list 1 2 3 4 5) 10)
