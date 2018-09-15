e5fda9dadecc8c1f36e170c99f1051d4254db4ad
spainisnotequal <spainisnotequal@gmail.com>
2018-09-15 19:05:15 +0200

    Add equality comparison for numbers in exercise 2.54

--- chapter2/exercise2.54.scm
+++ chapter2/exercise2.54.scm
@@ -6,6 +6,7 @@
 (define (equal? a b)
   (cond ((and (null? a) (null? b)) true)
 	((and (symbol? a) (symbol? b)) (eq? a b))
+	((and (number? a) (number? b)) (= a b))
 	((and (list? a) (list? b)) (and (equal? (car a) (car b))
 					(equal? (cdr a) (cdr b))))
 	(else false)))
@@ -13,3 +14,6 @@
 
 (equal? '(this is a list) '(this is a list))
 (equal? '(this is a list) '(this (is a) list))
+
+(equal? 3 3)
+(equal? 1 3)
