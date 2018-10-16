2d62a28e1af18af91e15e3fcf2a9919d38e3dd46
spainisnotequal <spainisnotequal@gmail.com>
2018-10-16 13:42:28 +0200

    Add examples of section 3.1.2

--- chapter3/section3.1.scm
+++ chapter3/section3.1.scm
@@ -2,7 +2,6 @@
 ;; Section 3.1: Assignament and Local State ;;
 ;; ======================================== ;;
 
-
 ;; ------------ ;;
 ;; Bank account ;;
 ;; ------------ ;;
@@ -29,3 +28,26 @@
 ((acc 'deposit) 40)
 ((acc 'withdraw) 60)
 
+;; --------------------------------- ;;
+;; Pi aproximation using Monte Carlo ;;
+;; --------------------------------- ;;
+
+(define (estimate-pi trials)
+  (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+
+(define (cesaro-test)
+  (= (gcd (rand) (rand)) 1))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0) (/ trials-passed trials))
+          ((experiment) (iter (- trials-remaining 1) (+ trials-passed 1)))
+          (else (iter (- trials-remaining 1) trials-passed))))
+  (iter trials 0))
+
+(define rand
+  (lambda () (random 999)))
+
+(estimate-pi 10)
+(estimate-pi 1000)
+(estimate-pi 100000)
