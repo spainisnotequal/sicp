33098dcf57f63e33e4266e300121fcf4773c36dc
spainisnotequal <spainisnotequal@gmail.com>
2018-11-27 12:24:45 +0100

    Add how to represent "queues" examples

--- chapter3/section3.3.scm
+++ chapter3/section3.3.scm
@@ -23,3 +23,62 @@ z1
 
 z2
 (set-to-wow! z2) ;=> ((wow b) a b)
+
+;; ------------------- ;;
+;; Representing Queues ;;
+;; ------------------- ;;
+
+;; constructor:
+
+(define (make-queue)
+  (cons '() '()))
+
+;; selectors:
+(define (empty-queue? queue)
+  (null? (front-ptr queue)))
+
+(define (front-ptr queue)
+  (car queue))
+
+(define (rear-ptr queue)
+  (cdr queue))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+;; mutators:
+(define (set-front-ptr! queue item)
+  (set-car! queue item))
+
+(define (set-rear-ptr! queue item)
+  (set-cdr! queue item))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else
+           (set-cdr! (rear-ptr queue) new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue))
+        (else
+         (set-front-ptr! queue (cdr (front-ptr queue)))
+         queue)))
+
+
+;; Examples:
+(define q (make-queue))
+(insert-queue! q 'a) ;=> ((a) a)
+(insert-queue! q 'b) ;=> ((a b) b)
+(delete-queue! q)    ;=> ((b) b)
+(insert-queue! q 'c) ;=> ((b c) c)
+(insert-queue! q 'd) ;=> ((b c d) d)
+(delete-queue! q)    ;=> ((c d) d)
